d1a8653dd589dd8e55d4568a412ae27b94ececda
Norotn <didokalexis@gmail.com>
2023-08-15 20:09:08 +0300

    make code cleaner

--- Homework3/task_2.py
+++ Homework3/task_2.py
@@ -14,7 +14,7 @@ URL = 'https://randomuser.me/api/?results=5000&format=csv'
 def setup_logger():
     logging.basicConfig(
         filename='my_log_file.log',
-        level=logging.DEBUG,  #
+        level=logging.DEBUG,
         format='%(asctime)s - %(levelname)s - %(message)s"'
     )
     return logging.getLogger('my_logger')
@@ -29,10 +29,10 @@ def download_user_accounts_csv(output_csv):
     if response.status_code == 200:
         data = response.text
         if data:
-            my_logger.info(f'Received user accounts from the API.')
-            with open(output_csv, 'w', newline='', encoding='utf-8') as file:
+            my_logger.info('Received user accounts from the API.')
+            with open(output_csv, 'w', encoding='utf-8') as file:
                 file.write(data)
-            my_logger.info('Successfully downloaded user accounts to user_accounts.csv')
+            my_logger.info(f'Successfully downloaded user accounts to {output_csv}')
         else:
             my_logger.warning('No data received from the API.')
     else:
@@ -40,81 +40,73 @@ def download_user_accounts_csv(output_csv):
 
 
 def read_data_from_csv():
-    with open('user_accounts.csv', 'r', newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        data = [row for row in reader]
-    return data
+    with open('user_accounts.csv', 'r', encoding='utf-8') as csvfile:
+        return list(csv.DictReader(csvfile))
 
 
-def write_data_in_csv(file_path, data1):
+def write_data_in_csv(file_path, input_data):
     with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = list(data1[0].keys())
+        fieldnames = list(input_data[0].keys())
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerows(data1)
+        writer.writerows(input_data)
         my_logger.info(f'Data was written in {file_path}.')
 
 
-def add_index_field(data1):
-    for index, data in enumerate(data1, start=1):
+def add_index_field(input_data):
+    for index, data in enumerate(input_data, start=1):
         data['global_index'] = index
-    return data1
+    return input_data
 
 
 def add_timezone_offset(datetime_str, offset_str):
-    offset_parts = offset_str.split(':')
-    hours = int(offset_parts[0])
-    minutes = int(offset_parts[1])
+    hours, minutes = map(int, offset_str.split(':'))
     offset = timedelta(hours=hours, minutes=minutes)
 
     dt_with_offset = datetime_str + offset
     return dt_with_offset.strftime('%Y-%m-%d %H:%M:%S')
 
 
-def add_current_time(data1):
-    for data in data1:
+def add_current_time(input_data):
+    for data in input_data:
         timezone_offset = data['location.timezone.offset']
         current_time = datetime.now()
         data['current_time'] = add_timezone_offset(current_time, timezone_offset)
     my_logger.info(f'Add current time field')
-    return data1
+    return input_data
 
 
-def change_name_content(data1):
+def change_name_content(input_data):
     rule_list = {'Mrs': 'missis', 'Ms ': 'miss', 'Mr': 'mister', 'Madame': 'mademoiselle'}
-    for data in data1:
-        if data['name.title'] in rule_list:
-            data['name.title'] = rule_list[data['name.title']]
-    my_logger.info(f'The content was changed in the field "name.title"')
-    return data1
+    my_logger.info('The content was changed in the field "name.title"')
+    return [{'name.title': rule_list.get(data['name.title'], data['name.title']), **data} for data in input_data]
 
 
-def convert_to_format(data1, field='dob.date', format_string='%Y-%m-%d'):
-    for data in data1:
+def convert_to_format(input_data, field='dob.date', format_string='%Y-%m-%d'):
+    for data in input_data:
         dob = datetime.strptime(data[field], '%Y-%m-%dT%H:%M:%S.%fZ')
         data[field] = dob.strftime(format_string)
     my_logger.info(f'Format of field {field} was changed to {format_string}')
-    return data1
+    return input_data
 
 
-def filter_data(data1, gender=None, num_rows=None):
-    filtered_data = []
+def filter_data(input_data, gender=None, num_rows=None):
     if gender:
-        filtered_data = [user for user in data1 if user['gender'] == gender]
-        # data1 = list(filter(lambda x: x['gender'] == gender, data1))
+        filtered_data = [user for user in input_data if user['gender'] == gender]
+        # input_data = list(filter(lambda x: x['gender'] == gender, input_data))
         my_logger.info('The data was filtered by gender')
-    if num_rows and num_rows < len(data1):
-        filtered_data = data1[:num_rows]
+    elif num_rows and num_rows < len(input_data):
+        filtered_data = input_data[:num_rows]
         my_logger.info('The data was filtered by num_rows')
-    if not gender and (not num_rows or num_rows >= len(data1)):  # почему ту не работает else?
+    else:
         my_logger.warning('The data was not filtered by any of the parameters!!!')
-        return data1
+        return input_data
     return filtered_data
 
 
 def check_folder_existence(path):
     if not os.path.exists(path):
-        os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
         my_logger.info(f'The folder was created along the path {path} ')
     os.chdir(path)
 
@@ -124,19 +116,14 @@ def move_file_to_destination(initial_file_path, destination_folder):
     my_logger.info(f'The {initial_file_path} was moved to {destination_folder} ')
 
 
-def rearrange_data(data1):
+def rearrange_data(input_data):
     new_structure = {}
-    for data in data1:
-        decade = str(int(int(data['dob.date'][-2:]) / 10)) + '0-th'
+    for data in input_data:
+        decade = f"{data['dob.date'][-2]}0-th"
         country = data.pop('location.country')
 
-        if decade not in new_structure:
-            new_structure[decade] = {}
+        new_structure.setdefault(decade, {}).setdefault(country, []).append(data)
 
-        if country not in new_structure[decade]:
-            new_structure[decade][country] = []
-
-        new_structure[decade][country].append(data)
     my_logger.info('The data was rearranged')
     return new_structure
 
@@ -150,7 +137,7 @@ def create_subfolders(destination_folder, data):
             country_folder = os.path.join(decade_folder, country)
             os.makedirs(country_folder, exist_ok=True)
             max_age = max([data['dob.age'] for data in country_data])
-            avg_registered = round(mean([int(data['registered.age']) for data in country_data]), 1)  # mean - avg
+            avg_registered = round(mean([int(data['registered.age']) for data in country_data]), 1)
             most_common_id = Counter(([data['id.name'] for data in country_data])).most_common(1)[0][0]
             filename = os.path.join(country_folder, f'{max_age}_{avg_registered}_{most_common_id}.csv')
             my_logger.info(f'Log paths {filename} was created.')
@@ -188,16 +175,24 @@ def preprocess_data(data, gender, num_rows):
     return filtered_data
 
 
+def create_parser():
+    parser = argparse.ArgumentParser(description='Download and filter user accounts data.')
+    parser.add_argument('--output_folder', type=str, required=True,
+                        help='Path to the destination folder where output file will be placed.')
+    parser.add_argument('--output_filename', default='output.csv', type=str, help='Output filename (default: output)')
+    parser.add_argument('--gender', choices=['male', 'female'], type=str,
+                        help='Filter data by gender (male or female).')
+    parser.add_argument('--num_rows', type=int, help='Filter data by number of rows.')
+    parser.add_argument('--log_level', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='INFO',
+                        type=str, help='Log level (default: INFO).')
+    return parser
+
+
 def main(output_folder, output_filename, log_lev, gender=None, num_rows=None):
     check_folder_existence(output_folder)
     my_logger.setLevel(log_lev)
 
     download_user_accounts_csv('user_accounts.csv')
-    print(f"Destination folder: {output_folder}")
-    print(f"Filename: {output_filename}")
-    print(f"Gender filter: {gender}")
-    print(f"Number of rows filter: {num_rows}")
-    print(f"Log level: {log_lev}")
 
     data = read_data_from_csv()
 
@@ -213,18 +208,8 @@ def main(output_folder, output_filename, log_lev, gender=None, num_rows=None):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Download and filter user accounts data.')
-    parser.add_argument('--output_folder', type=str,
-                        help='Path to the destination folder where output file will be placed.')
-    parser.add_argument('--output_filename', default='output.csv', type=str, help='Output filename (default: output)')
-    # parser.add_mutually_exclusive_group()  ? should i use it here? for --gender and --num_rows
-    parser.add_argument('--gender', choices=['male', 'female'], type=str,
-                        help='Filter data by gender (male or female).')
-    parser.add_argument('--num_rows', type=int, help='Filter data by number of rows.')
-    parser.add_argument('--log_level', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='INFO',
-                        type=str, help='Log level (default: INFO).')
-
-    args = parser.parse_args()
+    my_parser = create_parser()
+    args = my_parser.parse_args()
     log_level = getattr(logging, args.log_level.upper())
 
     main(args.output_folder, args.output_filename, log_level, args.gender, args.num_rows)
